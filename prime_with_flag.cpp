3c7c92b43c46565b1baa5a6f3e713d8c7588a054
Solleti Kowsalya <70649345+kowsalyaprasad98@users.noreply.github.com>
2021-06-07 18:07:29 +0530

    Create prime_with_flag.cpp

---
+++ prime_with_flag.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+   int num , flag = 0;
+   cin>>num;
+   for(int i=2 ; i < num/2 ; i++) {
+      if(num%i == 0) {
+         
+         if(flag = 1);
+         {
+             cout<<num<<"is not prime";
+             break;
+         }
+         
+      
+   else {
+      cout<<num<<"is prime";
+   }
+      }
+}
+  //another method
+  #include <bits/stdc++.h>
+using namespace std;
+int main() {
+   int num , flag = 0;
+   cin>>num;
+   for(int i=2 ; i < num ; i++) {
+      if(num%i == 0) {
+         
+         if(flag = 1);
+         {
+             cout<<num<<"is not prime";
+             break;
+         }
+         
+      
+   else {
+      cout<<num<<"is prime";
+   }
+      }
+}
+  // 3rd method
+  #include <bits/stdc++.h>
+using namespace std;
+int main() {
+   int num , flag = 0;
+   cin>>num;
+   for(int i=2 ; i < sqrt(num) ; i++) {
+      if(num%i == 0) {
+         
+         if(flag = 1);
+         {
+             cout<<num<<"is not prime";
+             break;
+         }
+         
+      
+   else {
+      cout<<num<<"is prime";
+   }
+      }
+}
